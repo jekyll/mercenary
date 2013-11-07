3e051950e2f8abfb75046df22829abb24282e354
Parker Moore <parkrmoore@gmail.com>
2013-11-07 13:06:21 -0500

    Fix load path problem in main mercenary.rb file

--- lib/mercenary.rb
+++ lib/mercenary.rb
@@ -1,4 +1,7 @@
-require "mercenary/version"
+lib = File.expand_path('../', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+
+require 'mercenary/version'
 require "optparse"
 
 module Mercenary
