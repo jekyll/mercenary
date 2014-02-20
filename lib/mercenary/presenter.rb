c5b3d7b6c0218197d58a8d16ec4c9c5594564e8d
Parker Moore <parkrmoore@gmail.com>
2014-02-20 17:50:33 -0500

    TomDoc Mercenary::Presenter

--- lib/mercenary/presenter.rb
+++ lib/mercenary/presenter.rb
@@ -2,30 +2,50 @@ module Mercenary
   class Presenter
     attr_accessor :command
 
+    # Public: Make a new Presenter
+    #
+    # command - a Mercenary::Command to present
+    #
+    # Returns nothing
     def initialize(command)
       @command = command
     end
 
+    # Public: Builds a string representation of the command usage
+    #
+    # Returns the string representation of the command usage
     def usage_presentation
       "  #{command.syntax}"
     end
 
+    # Public: Builds a string representation of the options
+    #
+    # Returns the string representation of the options
     def options_presentation
       return nil unless command.options.size > 0
       command.options.map(&:to_s).join("\n")
     end
 
+    # Public: Builds a string representation of the subcommands
+    #
+    # Returns the string representation of the subcommands
     def subcommands_presentation
       return nil unless command.commands.size > 0
       command.commands.values.map(&:summarize).join("\n")
     end
 
+    # Public: Builds the command header, including the command identity and description
+    #
+    # Returns the command header as a String
     def command_header
       header = "#{command.identity}"
       header << " -- #{command.description}" if command.description
       header
     end
 
+    # Public: Builds a string representation of the whole command
+    #
+    # Returns the string representation of the whole command
     def command_presentation
       msg = []
       msg << command_header
@@ -41,6 +61,13 @@ module Mercenary
       msg.join("\n\n")
     end
 
+    # Public: Turn a print_* into a *_presentation or freak out
+    #
+    # meth  - the method being called
+    # args  - an array of arguments passed to the missing method
+    # block - the block passed to the missing method
+    #
+    # Returns the value of whatever function is called
     def method_missing(meth, *args, &block)
       if meth.to_s =~ /^print_(.+)$/
         send("#{$1.downcase}_presentation")
