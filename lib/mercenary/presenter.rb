72c8b5371ae9be0e868e47d8f269b40efc38fdbf
Parker Moore <parkrmoore@gmail.com>
2014-02-20 15:49:21 -0500

    Do some String voodoo to get centering around the "," for the options

--- lib/mercenary/presenter.rb
+++ lib/mercenary/presenter.rb
@@ -12,13 +12,25 @@ module Mercenary
 
     def options_presentation
       return nil unless command.options.size > 0
-      command.options.map do |o|
-        pretty_options = if o.size > 2
-          o.take(2).join(", ")
-        else
-          o.first
+      command.options.map do |opt|
+        output = []
+        switches = opt.take(opt.size - 1)
+        if switches.size < 2
+          if switches.first.start_with?("--")
+            switches.unshift ""
+          else
+            switches << ""
+          end
         end
-        "#{pretty_options.rjust(20)}  #{o.last}"
+        output << switches.first.rjust(12)
+        if switches.first.empty?
+          output.first << " "
+        else
+          output.first << ","
+        end
+        output << switches.last.ljust(15)
+        output << opt.last
+        output.join(" ")
       end.join("\n")
     end
 
@@ -45,9 +57,8 @@ module Mercenary
       if meth.to_s =~ /^print_(.+)$/
         send("#{$1.downcase}_presentation")
       else
-        super # You *must* call super if you don't handle the
-              # method, otherwise you'll
-              # mess up Ruby's method lookup.
+        super # You *must* call super if you don't handle the method,
+              # otherwise you'll mess up Ruby's method lookup.
       end
     end
   end
